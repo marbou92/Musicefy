c893f23200b04bf6b3a073a1c873bfdea18b307e
marbou92 <mr.marb1930@gmail.com>
2026-05-11 18:47:58 +0100

    App.xaml.cs

--- Musicefy/App.xaml.cs
+++ Musicefy/App.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows;
+using Musicefy.Core; // Import ThemeManager
 
 namespace Musicefy
 {
@@ -9,32 +10,10 @@ namespace Musicefy
         {
             base.OnStartup(e);
 
-            // Load user preferences (theme, etc.)
-            string savedTheme = Properties.Settings.Default.Theme;
-            if (!string.IsNullOrEmpty(savedTheme))
-            {
-                ApplyTheme(savedTheme);
-            }
+            // Load saved theme or default
+            ThemeManager.LoadSavedTheme();
 
             // Future: Initialize logging, analytics, or dependency injection here
-        }
-
-        public static void ApplyTheme(string themeName)
-        {
-            string themePath = themeName switch
-            {
-                "Light" => "Themes/Light.xaml",
-                "Dark" => "Themes/Dark.xaml",
-                _ => "Themes/Dark.xaml"
-            };
-
-            var dict = new ResourceDictionary { Source = new Uri(themePath, UriKind.Relative) };
-            Current.Resources.MergedDictionaries.Clear();
-            Current.Resources.MergedDictionaries.Add(dict);
-
-            // Persist choice
-            Properties.Settings.Default.Theme = themeName;
-            Properties.Settings.Default.Save();
         }
     }
 }
