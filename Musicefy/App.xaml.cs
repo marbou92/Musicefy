4bd17b3b95e6afd30798157490d77ade1b05ee1d
marbou92 <mr.marb1930@gmail.com>
2026-05-11 23:17:24 +0100

    App.xaml.cs

--- Musicefy/App.xaml.cs
+++ Musicefy/App.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Windows;
 using System.Windows.Threading;
+using Musicefy.Services; // <-- Needed for ThemeManager
 
 namespace Musicefy
 {
@@ -14,6 +15,24 @@ namespace Musicefy
             // Global exception handlers
             this.DispatcherUnhandledException += App_DispatcherUnhandledException;
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            // Apply saved theme at startup
+            try
+            {
+                string savedTheme = Musicefy.Properties.Settings.Default.Theme;
+                if (!string.IsNullOrEmpty(savedTheme))
+                {
+                    ThemeManager.ApplyTheme(savedTheme);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogException(ex);
+                MessageBox.Show($"Theme load error: {ex.Message}\nSee crash.log for details.",
+                                "Musicefy Theme Error",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Warning);
+            }
         }
 
         private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
