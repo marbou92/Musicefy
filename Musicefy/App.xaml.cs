66addf2af4c28dc2cf9a499f9bea4a7a18680146
marbou92 <mr.marb1930@gmail.com>
2026-05-11 21:38:20 +0100

    App.xaml.cs

--- Musicefy/App.xaml.cs
+++ Musicefy/App.xaml.cs
@@ -5,6 +5,12 @@ namespace Musicefy
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ThemeManager.LoadSavedTheme();
+        }
+
         public static void ApplyTheme(string themeName)
         {
             ThemeManager.ApplyTheme(themeName);
