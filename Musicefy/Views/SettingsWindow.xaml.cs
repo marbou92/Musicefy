2135ea60188a230eced2d4848cb3cd4a602e221f
marbou92 <mr.marb1930@gmail.com>
2026-05-12 23:51:59 +0100

    Update SettingsWindow.xaml.cs

--- Musicefy/Views/SettingsWindow.xaml.cs
+++ Musicefy/Views/SettingsWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls;
 using Musicefy.Services;
 
 namespace Musicefy.Views
@@ -30,22 +31,27 @@ namespace Musicefy.Views
             InitializeComponent();
             DataContext = this;
 
+            // Load available themes (mode + palette combos)
             AvailableThemes = new ObservableCollection<string>(ThemeManager.GetAvailableThemes());
 
-            _originalTheme = Musicefy.Properties.Settings.Default.Theme ?? "Dark";
+            // Restore saved theme
+            _originalTheme = Musicefy.Properties.Settings.Default.Theme ?? "Dark|Default";
             SelectedTheme = _originalTheme;
 
-            ThemeManager.ApplyTheme(_originalTheme);
+            // Apply saved theme immediately
+            ApplyThemeFromString(_originalTheme);
         }
 
+        // Preview theme on hover
         private void ThemeCombo_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
         {
             if (ThemeCombo.SelectedItem is string hoveredTheme)
             {
-                ThemeManager.ApplyTheme(hoveredTheme);
+                ApplyThemeFromString(hoveredTheme);
             }
         }
 
+        // Save selected theme permanently
         private void Save_Click(object sender, RoutedEventArgs e)
         {
             ThemeManager.SaveTheme(SelectedTheme);
@@ -55,11 +61,22 @@ namespace Musicefy.Views
             Close();
         }
 
+        // Cancel and revert to original theme
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            ThemeManager.ApplyTheme(_originalTheme);
+            ApplyThemeFromString(_originalTheme);
             DialogResult = false;
             Close();
+        }
+
+        // Helper: parse "Mode|Palette" string and apply
+        private void ApplyThemeFromString(string themeString)
+        {
+            var parts = themeString.Split('|');
+            string mode = parts.Length > 0 ? parts[0] : "Dark";
+            string palette = parts.Length > 1 ? parts[1] : "Default";
+
+            ThemeManager.ApplyTheme(mode, palette);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
