aeb764d9e7ee9c0f3d08d23682b4b8c845467534
marbou92 <mr.marb1930@gmail.com>
2026-05-14 15:58:28 +0100

    Create LibraryControl.xaml.cs

---
+++ Musicefy/Views/LibraryControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Musicefy.Views
+{
+    public partial class LibraryControl : UserControl
+    {
+        public LibraryControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
