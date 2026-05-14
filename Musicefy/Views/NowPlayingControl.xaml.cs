f47bf5bbcb7a8405f6b4501ab87839a53500fe63
marbou92 <mr.marb1930@gmail.com>
2026-05-14 16:01:53 +0100

    Create NowPlayingControl.xaml.cs

---
+++ Musicefy/Views/NowPlayingControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Musicefy.Views
+{
+    public partial class NowPlayingControl : UserControl
+    {
+        public NowPlayingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
