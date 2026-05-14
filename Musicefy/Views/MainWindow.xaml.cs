5d783cea022275d18d4d49fdd964cf6e9bed36e2
marbou92 <mr.marb1930@gmail.com>
2026-05-14 16:11:57 +0100

    Update MainWindow.xaml.cs

--- Musicefy/Views/MainWindow.xaml.cs
+++ Musicefy/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Windows;
+using System.Windows.Media.Imaging;
 using Musicefy.Views;
 using Musicefy.Services;
 using Musicefy.Core.Models;
@@ -22,6 +24,7 @@ namespace Musicefy
             MainContent.Content = new HomeControl(_playback);
         }
 
+        // Navigation
         private void Home_Click(object sender, RoutedEventArgs e) => MainContent.Content = new HomeControl(_playback);
         private void Search_Click(object sender, RoutedEventArgs e) => MainContent.Content = new SearchControl(_playback);
         private void Library_Click(object sender, RoutedEventArgs e) => MainContent.Content = new LibraryControl(_playback);
@@ -33,15 +36,37 @@ namespace Musicefy
             win.ShowDialog();
         }
 
-        // Event handlers
+        // Mini Player updates
         private void OnTrackChanged(MusicFile track)
         {
-            // Could update a mini player bar here
+            MiniTitle.Text = track.Title;
+            MiniArtist.Text = track.Artist;
+
+            if (!string.IsNullOrEmpty(track.CoverPath))
+                MiniCover.Source = new BitmapImage(new Uri(track.CoverPath, UriKind.RelativeOrAbsolute));
+            else
+                MiniCover.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/default_cover.png"));
+
+            MiniProgress.Value = 0;
+            MiniProgress.Maximum = track.Duration.TotalSeconds;
         }
 
-        private void OnProgressChanged(System.TimeSpan current, System.TimeSpan total)
+        private void OnProgressChanged(TimeSpan current, TimeSpan total)
         {
-            // Could update a global progress bar here
+            MiniProgress.Maximum = total.TotalSeconds;
+            MiniProgress.Value = current.TotalSeconds;
+        }
+
+        // Controls
+        private void PlayButton_Click(object sender, RoutedEventArgs e) => _playback.Resume();
+        private void PauseButton_Click(object sender, RoutedEventArgs e) => _playback.Pause();
+        private void NextButton_Click(object sender, RoutedEventArgs e) => _playback.Next();
+        private void PreviousButton_Click(object sender, RoutedEventArgs e) => _playback.Previous();
+
+        private void MiniProgress_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (_playback.CurrentAudioFile != null)
+                _playback.Seek(TimeSpan.FromSeconds(MiniProgress.Value));
         }
     }
 }
