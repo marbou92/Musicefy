66214078569305a824868fe07dc8d7e8629ea452
marbou92 <mr.marb1930@gmail.com>
2026-05-14 22:36:21 +0100

    Update MainViewModel.cs

--- Musicefy/ViewModels/MainViewModel.cs
+++ Musicefy/ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -10,77 +9,38 @@ namespace Musicefy.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<MusicFile> Favourites { get; set; }
-        public ObservableCollection<MusicFile> Downloads { get; set; }
-        public ObservableCollection<MusicFile> History { get; set; }
-
-        private MusicFile _nowPlaying;
-        public MusicFile NowPlaying 
-        { 
-            get => _nowPlaying; 
-            set { _nowPlaying = value; OnPropertyChanged(); } 
-        }
-
         public ObservableCollection<CategoryItem> HeaderCategories { get; set; }
         public ObservableCollection<ChartCard> BrowseCharts { get; set; }
         public ObservableCollection<TrackCard> QuickPicks { get; set; }
-        public ObservableCollection<VideoCard> TopMusicVideos { get; set; }
+
+        private MusicFile _nowPlaying;
+        public MusicFile NowPlaying { get => _nowPlaying; set { _nowPlaying = value; OnPropertyChanged(); } }
 
         public MainViewModel()
         {
-            InitializeData();
+            HeaderCategories = new ObservableCollection<CategoryItem> {
+                new CategoryItem { Name = "Energize" }, new CategoryItem { Name = "Relax" }, 
+                new CategoryItem { Name = "Focus" }, new CategoryItem { Name = "Romance" }
+            };
+
+            var placeholder = new BitmapImage(new Uri("pack://application:,,,/Assets/default_cover.png"));
+
+            BrowseCharts = new ObservableCollection<ChartCard> {
+                new ChartCard { Title = "Global Top 50", Subtitle = "Your daily update", Cover = placeholder },
+                new ChartCard { Title = "Viral Hits", Subtitle = "Trending on Musicefy", Cover = placeholder }
+            };
+
+            QuickPicks = new ObservableCollection<TrackCard> {
+                new TrackCard { Title = "Moonlight", Artist = "Lofi Girl", Cover = placeholder },
+                new TrackCard { Title = "Midnight City", Artist = "M83", Cover = placeholder }
+            };
         }
-
-        private void InitializeData()
-        {
-            var defaultCover = new BitmapImage(new Uri("pack://application:,,,/Assets/default_cover.png"));
-
-            // 1. Categories
-            HeaderCategories = new ObservableCollection<CategoryItem>
-            {
-                new CategoryItem { Name = "Podcasts" }, new CategoryItem { Name = "Work out" },
-                new CategoryItem { Name = "Feel good" }, new CategoryItem { Name = "Romance" },
-                new CategoryItem { Name = "Relax" }, new CategoryItem { Name = "Focus" }
-            };
-
-            // 2. Mock Charts
-            BrowseCharts = new ObservableCollection<ChartCard>
-            {
-                new ChartCard { Title = "Global Top 50", Subtitle = "Daily updates", Cover = defaultCover },
-                new ChartCard { Title = "Viral 50", Subtitle = "Trending now", Cover = defaultCover }
-            };
-
-            // 3. Quick Picks
-            QuickPicks = new ObservableCollection<TrackCard>
-            {
-                new TrackCard { Title = "Sahiba", Artist = "Sahiba", Cover = defaultCover },
-                new TrackCard { Title = "Pal Pal", Artist = "Tajwinder", Cover = defaultCover }
-            };
-
-            // 4. Videos
-            TopMusicVideos = new ObservableCollection<VideoCard>
-            {
-                new VideoCard { Title = "New Release", Channel = "Official Vevo", Cover = defaultCover }
-            };
-
-            // Existing placeholder logic
-            Favourites = new ObservableCollection<MusicFile>();
-            History = new ObservableCollection<MusicFile>();
-            Downloads = new ObservableCollection<MusicFile>();
-        }
-
-        private string EnsureArtist(string artist) => string.IsNullOrWhiteSpace(artist) ? "Unknown" : artist;
 
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 
-    // Support Models
     public class CategoryItem { public string Name { get; set; } }
     public class ChartCard { public string Title { get; set; } public string Subtitle { get; set; } public BitmapImage Cover { get; set; } }
     public class TrackCard { public string Title { get; set; } public string Artist { get; set; } public BitmapImage Cover { get; set; } }
-    public class VideoCard { public string Title { get; set; } public string Channel { get; set; } public BitmapImage Cover { get; set; } }
 }
