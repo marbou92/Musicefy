19671bab735cbd8db328ae902b616dd4bff2fbdd
marbou92 <mr.marb1930@gmail.com>
2026-05-14 16:25:19 +0100

    Update NowPlayingControl.xaml.cs

--- Musicefy/Views/NowPlayingControl.xaml.cs
+++ Musicefy/Views/NowPlayingControl.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using Musicefy.Services;
 using Musicefy.Core.Models;
@@ -10,6 +11,9 @@ namespace Musicefy.Views
     {
         private readonly PlaybackService _playback;
 
+        // Event to notify MainWindow to collapse back
+        public event Action RequestCollapse;
+
         public NowPlayingControl(PlaybackService playback)
         {
             InitializeComponent();
@@ -17,8 +21,28 @@ namespace Musicefy.Views
 
             _playback.TrackChanged += OnTrackChanged;
             _playback.ProgressChanged += OnProgressChanged;
+
+            // Enable swipe gesture
+            this.ManipulationMode = ManipulationModes.TranslateY;
+            this.ManipulationDelta += OnManipulationDelta;
         }
 
+        // Swipe down gesture
+        private void OnManipulationDelta(object sender, ManipulationDeltaEventArgs e)
+        {
+            if (e.DeltaManipulation.Translation.Y > 50) // detect downward swipe
+            {
+                RequestCollapse?.Invoke();
+            }
+        }
+
+        // Back button click
+        private void BackButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            RequestCollapse?.Invoke();
+        }
+
+        // Update UI when track changes
         private void OnTrackChanged(MusicFile track)
         {
             Title.Text = track.Title;
@@ -38,11 +62,15 @@ namespace Musicefy.Views
             }
         }
 
+        // Update progress (hook to slider if needed)
         private void OnProgressChanged(TimeSpan current, TimeSpan total)
         {
-            // Could update a progress bar here if you add one
+            // Example: if you add a progress bar in XAML
+            // ProgressSlider.Maximum = total.TotalSeconds;
+            // ProgressSlider.Value = current.TotalSeconds;
         }
 
+        // Playback controls
         private void Play_Click(object sender, System.Windows.RoutedEventArgs e) => _playback.Resume();
         private void Pause_Click(object sender, System.Windows.RoutedEventArgs e) => _playback.Pause();
         private void Next_Click(object sender, System.Windows.RoutedEventArgs e) => _playback.Next();
