dfa11c27122e98bbcb26b4e7cda2f3e8fefe1299
marbou92 <mr.marb1930@gmail.com>
2026-05-14 16:06:22 +0100

    Update SearchControl.xaml.cs

--- Musicefy/Views/SearchControl.xaml.cs
+++ Musicefy/Views/SearchControl.xaml.cs
@@ -1,17 +1,33 @@
 using System.Windows.Controls;
+using Musicefy.Services;
+using Musicefy.Core.Models;
 
 namespace Musicefy.Views
 {
     public partial class SearchControl : UserControl
     {
-        public SearchControl()
+        private readonly PlaybackService _playback;
+
+        public SearchControl(PlaybackService playback)
         {
             InitializeComponent();
+            _playback = playback;
         }
 
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            // Hook into your source manager to filter tracks
+            // Example: filter your library
+            var query = SearchBox.Text.ToLower();
+            var results = MusicefyApp.Library
+                .Where(t => t.Title.ToLower().Contains(query) || t.Artist.ToLower().Contains(query))
+                .ToList();
+            SearchResults.ItemsSource = results;
+        }
+
+        private void SearchResults_DoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (SearchResults.SelectedItem is MusicFile track)
+                _playback.PlayTrack(track);
         }
     }
 }
