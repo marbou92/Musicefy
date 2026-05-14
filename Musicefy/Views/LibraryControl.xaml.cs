0c806724f87e48472e5483ec3988151197e53198
marbou92 <mr.marb1930@gmail.com>
2026-05-14 16:06:34 +0100

    Update LibraryControl.xaml.cs

--- Musicefy/Views/LibraryControl.xaml.cs
+++ Musicefy/Views/LibraryControl.xaml.cs
@@ -1,12 +1,24 @@
 using System.Windows.Controls;
+using Musicefy.Services;
+using Musicefy.Core.Models;
 
 namespace Musicefy.Views
 {
     public partial class LibraryControl : UserControl
     {
-        public LibraryControl()
+        private readonly PlaybackService _playback;
+
+        public LibraryControl(PlaybackService playback)
         {
             InitializeComponent();
+            _playback = playback;
+            LibraryList.ItemsSource = MusicefyApp.Library;
+        }
+
+        private void LibraryList_DoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (LibraryList.SelectedItem is MusicFile track)
+                _playback.PlayTrack(track);
         }
     }
 }
