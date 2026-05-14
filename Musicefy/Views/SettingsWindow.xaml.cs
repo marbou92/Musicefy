aa2d776896a3a0c297eb1bc504875cf65f8aedff
marbou92 <mr.marb1930@gmail.com>
2026-05-14 12:09:43 +0100

    Update SettingsWindow.xaml.cs

--- Musicefy/Views/SettingsWindow.xaml.cs
+++ Musicefy/Views/SettingsWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 using Musicefy.ViewModels;
 
 namespace Musicefy.Views
@@ -15,28 +17,63 @@ namespace Musicefy.Views
 
         private void AppearanceButton_Click(object sender, RoutedEventArgs e)
         {
+            AppearanceButton.IsChecked = true;
+            DownloadsButton.IsChecked = false;
             ShowAppearance();
         }
 
         private void DownloadsButton_Click(object sender, RoutedEventArgs e)
         {
+            DownloadsButton.IsChecked = true;
+            AppearanceButton.IsChecked = false;
             ShowDownloads();
         }
 
         private void ShowAppearance()
         {
             _appearanceVM = new AppearanceSettingsViewModel();
-            SettingsContent.Content = new AppearanceSettingsControl
+            var control = new AppearanceSettingsControl
             {
                 DataContext = _appearanceVM
             };
-            SectionTitle.Text = "Appearance Settings";
+
+            AnimateContentChange(control, "Appearance Settings");
         }
 
         private void ShowDownloads()
         {
-            SettingsContent.Content = new DownloadsSettingsControl();
-            SectionTitle.Text = "Downloads Settings";
+            var control = new DownloadsSettingsControl();
+            AnimateContentChange(control, "Downloads Settings");
+        }
+
+        /// <summary>
+        /// Handles fade animation when swapping content.
+        /// </summary>
+        private void AnimateContentChange(UserControl newContent, string title)
+        {
+            // Fade out current content
+            var fadeOut = new DoubleAnimation(1, 0, new Duration(System.TimeSpan.FromMilliseconds(200)));
+            fadeOut.Completed += (s, e) =>
+            {
+                SettingsContent.Content = newContent;
+                SectionTitle.Text = title;
+
+                // Fade in new content
+                var fadeIn = new DoubleAnimation(0, 1, new Duration(System.TimeSpan.FromMilliseconds(200)));
+                newContent.BeginAnimation(OpacityProperty, fadeIn);
+            };
+
+            if (SettingsContent.Content is FrameworkElement currentContent)
+            {
+                currentContent.BeginAnimation(OpacityProperty, fadeOut);
+            }
+            else
+            {
+                SettingsContent.Content = newContent;
+                SectionTitle.Text = title;
+                var fadeIn = new DoubleAnimation(0, 1, new Duration(System.TimeSpan.FromMilliseconds(200)));
+                newContent.BeginAnimation(OpacityProperty, fadeIn);
+            }
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
@@ -48,7 +85,6 @@ namespace Musicefy.Views
             }
             else if (SettingsContent.Content is DownloadsSettingsControl downloadsControl)
             {
-                // trigger save logic in DownloadsSettingsControl
                 downloadsControl.GetType().GetMethod("Save_Click")?
                     .Invoke(downloadsControl, new object[] { sender, e });
             }
