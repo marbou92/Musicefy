5e3d5565a9104754a21a76e49142f98ee9d66523
marbou92 <mr.marb1930@gmail.com>
2026-05-16 23:34:24 +0100

    Update PathToImageConverter.cs

--- Musicefy/Converters/PathToImageConverter.cs
+++ Musicefy/Converters/PathToImageConverter.cs
@@ -10,18 +10,29 @@ namespace Musicefy.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string path = value as string;
-            if (string.IsNullOrEmpty(path))
-            {
-                return new BitmapImage(new Uri("pack://application:,,,/Assets/default_cover.png"));
-            }
+            return CreateBitmap(string.IsNullOrEmpty(path) ? "pack://application:,,,/Assets/default_cover.png" : path);
+        }
 
+        private BitmapImage CreateBitmap(string uriString)
+        {
             try
             {
-                return new BitmapImage(new Uri(path, UriKind.RelativeOrAbsolute));
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(uriString, UriKind.RelativeOrAbsolute);
+                // PERFORMANCE FIX: CacheOnLoad prevents file locking and drops memory allocation spikes on win7
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze(); // Freeze to allow cross-thread UI access safely
+                return bitmap;
             }
             catch
             {
-                return new BitmapImage(new Uri("pack://application:,,,/Assets/default_cover.png"));
+                if (uriString != "pack://application:,,,/Assets/default_cover.png")
+                {
+                    return CreateBitmap("pack://application:,,,/Assets/default_cover.png");
+                }
+                return null;
             }
         }
 
