8ce64ac69246646e238fbcd11265a25577c4090f
marbou92 <mr.marb1930@gmail.com>
2026-05-16 14:22:59 +0100

    Update AppearanceSettingsControl.xaml.cs

--- Musicefy/Views/AppearanceSettingsControl.xaml.cs
+++ Musicefy/Views/AppearanceSettingsControl.xaml.cs
@@ -1,11 +1,12 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Musicefy.Core.Interfaces;
 using Musicefy.ViewModels;
 
 namespace Musicefy.Views
 {
-    public partial class AppearanceSettingsControl : UserControl
+    public partial class AppearanceSettingsControl : UserControl, ISettingsControl
     {
         private readonly AppearanceSettingsViewModel _viewModel;
 
@@ -16,20 +17,31 @@ namespace Musicefy.Views
             this.DataContext = _viewModel;
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
+        // ISettingsControl Implementation
+        public void Save()
         {
             _viewModel.Save();
+        }
+
+        public void Cancel()
+        {
+            _viewModel.Cancel();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Save();
             MessageBox.Show("Appearance settings saved.", "Musicefy", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
-            _viewModel.Cancel();
+            Cancel();
         }
 
         private void PaletteCard_Click(object sender, MouseButtonEventArgs e)
         {
-            if (sender is Border border && border.DataContext is ThemePreview preview)
+            if (sender is FrameworkElement element && element.DataContext is ThemePreview preview)
             {
                 _viewModel.SelectPalette(preview.CardName);
             }
