1ce1882dcd2acc52f27059826fb978034d73f688
marbou92 <mr.marb1930@gmail.com>
2026-05-16 14:16:29 +0100

    Update App.xaml.cs

--- Musicefy/App.xaml.cs
+++ Musicefy/App.xaml.cs
@@ -68,7 +68,7 @@ namespace Musicefy
             }
             catch
             {
-                // swallow logging errors
+                // swallow logging errors safely
             }
         }
     }
