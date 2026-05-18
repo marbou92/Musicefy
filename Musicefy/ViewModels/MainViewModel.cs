45db3647c315ada877764c5a3936cba74eb57b36
marbou92 <mr.marb1930@gmail.com>
2026-05-18 11:21:00 +0100

    Update MainViewModel.cs

--- Musicefy/ViewModels/MainViewModel.cs
+++ Musicefy/ViewModels/MainViewModel.cs
@@ -12,8 +12,6 @@ namespace Musicefy.ViewModels
         public ObservableCollection<CategoryItem> HeaderCategories { get; set; }
         public ObservableCollection<ChartCard> BrowseCharts { get; set; }
         public ObservableCollection<TrackCard> QuickPicks { get; set; }
-        
-        // FIXED: Added the missing collection for the VideosList
         public ObservableCollection<VideoCard> TopMusicVideos { get; set; }
 
         private MusicFile _nowPlaying;
@@ -37,7 +35,6 @@ namespace Musicefy.ViewModels
                 new TrackCard { Title = "Midnight City", Artist = "M83", Cover = placeholder }
             };
 
-            // FIXED: Initialized the Video collection
             TopMusicVideos = new ObservableCollection<VideoCard> {
                 new VideoCard { Title = "Lofi Hip Hop Radio", Channel = "Lofi Girl", Cover = placeholder },
                 new VideoCard { Title = "Synthwave Mix 2026", Channel = "Echo Beats", Cover = placeholder }
@@ -49,11 +46,8 @@ namespace Musicefy.ViewModels
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 
-    // Model Classes
     public class CategoryItem { public string Name { get; set; } }
     public class ChartCard { public string Title { get; set; } public string Subtitle { get; set; } public BitmapImage Cover { get; set; } }
     public class TrackCard { public string Title { get; set; } public string Artist { get; set; } public BitmapImage Cover { get; set; } }
-    
-    // FIXED: Added VideoCard class to match the ViewModel property
     public class VideoCard { public string Title { get; set; } public string Channel { get; set; } public BitmapImage Cover { get; set; } }
 }
