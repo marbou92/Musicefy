16d8aa87c324ef7c1d03403e63a17ba8e39fc79a
marbou92 <mr.marb1930@gmail.com>
2026-05-27 14:56:43 +0100

    Create SourceTypes.cs

---
+++ Musicefy.Core/SourceTypes.cs
@@ -0,0 +1,10 @@
+namespace Musicefy.Core
+{
+    public static class SourceTypes
+    {
+        public const string Local = "Local";
+        public const string YouTube = "YouTube";
+        public const string Subsonic = "Subsonic";
+        public const string Extension = "Extension";
+    }
+}
