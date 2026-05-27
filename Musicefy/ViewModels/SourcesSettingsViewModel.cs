2048286870c58d46beb5f1041f79f218f6047c8f
marbou92 <mr.marb1930@gmail.com>
2026-05-27 18:08:49 +0100

    Update SourcesSettingsViewModel.cs

--- Musicefy/ViewModels/SourcesSettingsViewModel.cs
+++ Musicefy/ViewModels/SourcesSettingsViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Extensions.DependencyInjection;
+using Musicefy.Core;
 using Musicefy.Core.Interfaces;
 using Musicefy.Core.Models;
 using Musicefy.Properties;
