8b2ea824e5796d818a0427881b455880fe81f4cb
marbou92 <mr.marb1930@gmail.com>
2026-05-27 17:59:24 +0100

    Update AppearanceSettingsViewModel.cs

--- Musicefy/ViewModels/AppearanceSettingsViewModel.cs
+++ Musicefy/ViewModels/AppearanceSettingsViewModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Media;
 using Musicefy.Services;
