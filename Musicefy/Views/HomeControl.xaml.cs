dce6516b16a91af8fe40ae9924aa19785ac91df5
marbou92 <mr.marb1930@gmail.com>
2026-05-28 23:11:28 +0100

    Update HomeControl.xaml.cs

--- Musicefy/Views/HomeControl.xaml.cs
+++ Musicefy/Views/HomeControl.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Animation;
 using Musicefy.Services;
 using Musicefy.ViewModels;
@@ -84,25 +85,51 @@ namespace Musicefy.Views
         private void StartShimmer()
         {
             if (_shimmerStoryboard != null) return;
-            _shimmerStoryboard = new Storyboard();
-            var pulse = new DoubleAnimation
+
+            var brush = new LinearGradientBrush
             {
-                From = 0.2,
-                To = 0.6,
-                Duration = new Duration(TimeSpan.FromSeconds(1.4)),
-                AutoReverse = true,
-                RepeatBehavior = RepeatBehavior.Forever
+                StartPoint = new Point(-1, 0),
+                EndPoint = new Point(0, 0)
             };
-            Storyboard.SetTargetProperty(pulse, new PropertyPath("Opacity"));
-            _shimmerStoryboard.Children.Add(pulse);
-            _shimmerStoryboard.Begin(LoadingSkeleton, true);
+            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x1A, 0xFF, 0xFF, 0xFF), 0.0));
+            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x4D, 0xFF, 0xFF, 0xFF), 0.5));
+            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x1A, 0xFF, 0xFF, 0xFF), 1.0));
+
+            ShimmerOverlay.Fill = brush;
+
+            _shimmerStoryboard = new Storyboard { RepeatBehavior = RepeatBehavior.Forever };
+
+            var startPointAnim = new PointAnimation
+            {
+                From = new Point(-1, 0),
+                To = new Point(1, 0),
+                Duration = TimeSpan.FromSeconds(2),
+                AutoReverse = true
+            };
+            Storyboard.SetTarget(startPointAnim, brush);
+            Storyboard.SetTargetProperty(startPointAnim, new PropertyPath(LinearGradientBrush.StartPointProperty));
+
+            var endPointAnim = new PointAnimation
+            {
+                From = new Point(0, 0),
+                To = new Point(2, 0),
+                Duration = TimeSpan.FromSeconds(2),
+                AutoReverse = true
+            };
+            Storyboard.SetTarget(endPointAnim, brush);
+            Storyboard.SetTargetProperty(endPointAnim, new PropertyPath(LinearGradientBrush.EndPointProperty));
+
+            _shimmerStoryboard.Children.Add(startPointAnim);
+            _shimmerStoryboard.Children.Add(endPointAnim);
+            _shimmerStoryboard.Begin(ShimmerOverlay, true);
         }
 
         private void StopShimmer()
         {
             if (_shimmerStoryboard == null) return;
-            _shimmerStoryboard.Stop(LoadingSkeleton);
+            _shimmerStoryboard.Stop(ShimmerOverlay);
             _shimmerStoryboard = null;
+            ShimmerOverlay.Fill = null;
         }
 
         private void QuickPicksList_DoubleClick(object sender, MouseButtonEventArgs e)
