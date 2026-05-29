f599a517a2df5d425f6bfe95c67f22b636bcd93c
marbou92 <mr.marb1930@gmail.com>
2026-05-29 12:19:27 +0100

    Update HomeControl.xaml.cs

--- Musicefy/Views/HomeControl.xaml.cs
+++ Musicefy/Views/HomeControl.xaml.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 using Musicefy.Services;
 using Musicefy.ViewModels;
 
@@ -15,7 +13,6 @@ namespace Musicefy.Views
     {
         private readonly PlaybackService _playback;
         private readonly MainViewModel _viewModel;
-        private Storyboard _shimmerStoryboard;
 
         public HomeControl(PlaybackService playback, MainViewModel mainViewModel)
         {
@@ -45,7 +42,6 @@ namespace Musicefy.Views
         private void OnUnloaded(object sender, RoutedEventArgs e)
         {
             _viewModel.PropertyChanged -= OnViewModelPropertyChanged;
-            StopShimmer();
         }
 
         private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -64,72 +60,19 @@ namespace Musicefy.Views
                 LoadingSkeleton.Visibility = Visibility.Visible;
                 ContentArea.Visibility = Visibility.Collapsed;
                 EmptyState.Visibility = Visibility.Collapsed;
-                StartShimmer();
             }
             else if (_viewModel.IsEmpty)
             {
                 EmptyState.Visibility = Visibility.Visible;
                 ContentArea.Visibility = Visibility.Collapsed;
                 LoadingSkeleton.Visibility = Visibility.Collapsed;
-                StopShimmer();
             }
             else
             {
                 ContentArea.Visibility = Visibility.Visible;
                 EmptyState.Visibility = Visibility.Collapsed;
                 LoadingSkeleton.Visibility = Visibility.Collapsed;
-                StopShimmer();
             }
-        }
-
-        private void StartShimmer()
-        {
-            if (_shimmerStoryboard != null) return;
-
-            var brush = new LinearGradientBrush
-            {
-                StartPoint = new Point(-1, 0),
-                EndPoint = new Point(0, 0)
-            };
-            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x1A, 0xFF, 0xFF, 0xFF), 0.0));
-            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x4D, 0xFF, 0xFF, 0xFF), 0.5));
-            brush.GradientStops.Add(new GradientStop(Color.FromArgb(0x1A, 0xFF, 0xFF, 0xFF), 1.0));
-
-            ShimmerOverlay.Fill = brush;
-
-            _shimmerStoryboard = new Storyboard { RepeatBehavior = RepeatBehavior.Forever };
-
-            var startPointAnim = new PointAnimation
-            {
-                From = new Point(-1, 0),
-                To = new Point(1, 0),
-                Duration = TimeSpan.FromSeconds(2),
-                AutoReverse = true
-            };
-            Storyboard.SetTarget(startPointAnim, brush);
-            Storyboard.SetTargetProperty(startPointAnim, new PropertyPath(LinearGradientBrush.StartPointProperty));
-
-            var endPointAnim = new PointAnimation
-            {
-                From = new Point(0, 0),
-                To = new Point(2, 0),
-                Duration = TimeSpan.FromSeconds(2),
-                AutoReverse = true
-            };
-            Storyboard.SetTarget(endPointAnim, brush);
-            Storyboard.SetTargetProperty(endPointAnim, new PropertyPath(LinearGradientBrush.EndPointProperty));
-
-            _shimmerStoryboard.Children.Add(startPointAnim);
-            _shimmerStoryboard.Children.Add(endPointAnim);
-            _shimmerStoryboard.Begin(ShimmerOverlay, true);
-        }
-
-        private void StopShimmer()
-        {
-            if (_shimmerStoryboard == null) return;
-            _shimmerStoryboard.Stop(ShimmerOverlay);
-            _shimmerStoryboard = null;
-            ShimmerOverlay.Fill = null;
         }
 
         private void QuickPicksList_DoubleClick(object sender, MouseButtonEventArgs e)
