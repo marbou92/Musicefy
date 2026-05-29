14aa94c94c67e9b1b844266f8939b626cae6bcd2
marbou92 <mr.marb1930@gmail.com>
2026-05-29 17:33:32 +0100

    Update ArtistView.xaml.cs

--- Musicefy/Views/ArtistView.xaml.cs
+++ Musicefy/Views/ArtistView.xaml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Musicefy.Core.Models;
+using Musicefy.Core.Services;
 using Musicefy.ViewModels;
 
 namespace Musicefy.Views
@@ -21,6 +24,38 @@ namespace Musicefy.Views
         public async void LoadArtist(string artistName)
         {
             await _viewModel.LoadAsync(artistName);
+            ExtractColorsFromCover();
+        }
+
+        private void ExtractColorsFromCover()
+        {
+            try
+            {
+                string coverPath = _viewModel.CoverPath;
+                if (string.IsNullOrEmpty(coverPath) || coverPath.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                    return;
+
+                if (!System.IO.File.Exists(coverPath))
+                    return;
+
+                var cover = new BitmapImage(new Uri(coverPath, UriKind.Absolute));
+                var colors = ColorExtractor.Extract(cover);
+
+                var gradient = new LinearGradientBrush
+                {
+                    StartPoint = new Point(0, 0),
+                    EndPoint = new Point(0, 1)
+                };
+                gradient.GradientStops.Add(new GradientStop(colors.Primary, 0.0));
+                gradient.GradientStops.Add(new GradientStop(colors.Surface, 0.5));
+                gradient.GradientStops.Add(new GradientStop(Color.FromRgb(18, 18, 18), 1.0));
+                gradient.Freeze();
+                _viewModel.BackgroundGradient = gradient;
+            }
+            catch
+            {
+                // Color extraction is non-critical
+            }
         }
 
         private void BtnBack_Click(object sender, RoutedEventArgs e)
