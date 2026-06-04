7048f532c261fadedc47eb2df8f859959a225fc3
marbou92 <mr.marb1930@gmail.com>
2026-06-04 21:22:39 +0100

    Add using directive for System.Threading.Tasks

--- Musicefy/Services/PlaybackService.cs
+++ Musicefy/Services/PlaybackService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
 using Musicefy.Core.Interfaces;
